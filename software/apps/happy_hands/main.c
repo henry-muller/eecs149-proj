ae710a512a67bfca5be8bb9ee22c451ac12db08e
Henry Muller <henrymuller@berkeley.edu>
2018-10-30 17:51:18 -0700

    Read in ADC values from flex sensor

--- software/apps/happy_hands/main.c
+++ software/apps/happy_hands/main.c
@@ -1,6 +1,6 @@
-// Analog accelerometer app
+// testing flex sensors
 //
-// Reads data from the ADXL327 analog accelerometer
+//
 
 #include <stdbool.h>
 #include <stdint.h>
@@ -20,73 +20,52 @@
 
 #include "buckler.h"
 
-// ADC channels
-#define X_CHANNEL 0
-#define Y_CHANNEL 1
-#define Z_CHANNEL 2
+#define ADC_CHANNEL 0
 
 // callback for SAADC events
 void saadc_callback (nrfx_saadc_evt_t const * p_event) {
-  // don't care about adc callbacks
+    // don't care about adc callbacks
 }
 
 // sample a particular analog channel in blocking mode
 nrf_saadc_value_t sample_value (uint8_t channel) {
-  nrf_saadc_value_t val;
-  ret_code_t error_code = nrfx_saadc_sample_convert(channel, &val);
-  APP_ERROR_CHECK(error_code);
-  return val;
+    nrf_saadc_value_t val;
+    ret_code_t error_code = nrfx_saadc_sample_convert(channel, &val);
+    APP_ERROR_CHECK(error_code);
+    return val;
 }
 
-int main (void) {
-  ret_code_t error_code = NRF_SUCCESS;
+int main() {
+    ret_code_t error_code = NRF_SUCCESS;
 
-  // initialize RTT library
-  error_code = NRF_LOG_INIT(NULL);
-  APP_ERROR_CHECK(error_code);
-  NRF_LOG_DEFAULT_BACKENDS_INIT();
+    // initialize RTT library
+    error_code = NRF_LOG_INIT(NULL);
+    APP_ERROR_CHECK(error_code);
+    NRF_LOG_DEFAULT_BACKENDS_INIT();
 
-  // initialize analog to digital converter
-  nrfx_saadc_config_t saadc_config = NRFX_SAADC_DEFAULT_CONFIG;
-  saadc_config.resolution = NRF_SAADC_RESOLUTION_12BIT;
-  error_code = nrfx_saadc_init(&saadc_config, saadc_callback);
-  APP_ERROR_CHECK(error_code);
+    // initialize analog to digital converter
+    nrfx_saadc_config_t saadc_config = NRFX_SAADC_DEFAULT_CONFIG;
+    saadc_config.resolution = NRF_SAADC_RESOLUTION_12BIT;
+    error_code = nrfx_saadc_init(&saadc_config, saadc_callback);
+    APP_ERROR_CHECK(error_code);
 
-  // initialize analog inputs
-  // configure with 0 as input pin for now
-  nrf_saadc_channel_config_t channel_config = NRFX_SAADC_DEFAULT_CHANNEL_CONFIG_SE(0);
-  channel_config.gain = NRF_SAADC_GAIN1_6; // input gain of 1/6 Volts/Volt, multiply incoming signal by (1/6)
-  channel_config.reference = NRF_SAADC_REFERENCE_INTERNAL; // 0.6 Volt reference, input after gain can be 0 to 0.6 Volts
+    // initialize analog inputs
+    // configure with 0 as input pin for now
+    nrf_saadc_channel_config_t channel_config = NRFX_SAADC_DEFAULT_CHANNEL_CONFIG_SE(0);
+    channel_config.gain = NRF_SAADC_GAIN1_6; // input gain of 1/6 Volts/Volt, multiply incoming signal by (1/6)
+    channel_config.reference = NRF_SAADC_REFERENCE_INTERNAL; // 0.6 Volt reference, input after gain can be 0 to 0.6 Volts
 
-  // specify input pin and initialize that ADC channel
-  channel_config.pin_p = BUCKLER_ANALOG_ACCEL_X;
-  error_code = nrfx_saadc_channel_init(X_CHANNEL, &channel_config);
-  APP_ERROR_CHECK(error_code);
+     // specify input pin and initialize that ADC channel
+    channel_config.pin_p = NRF_SAADC_INPUT_AIN0;
+    error_code = nrfx_saadc_channel_init(ADC_CHANNEL, &channel_config);
+    APP_ERROR_CHECK(error_code);
 
-  // specify input pin and initialize that ADC channel
-  channel_config.pin_p = BUCKLER_ANALOG_ACCEL_Y;
-  error_code = nrfx_saadc_channel_init(Y_CHANNEL, &channel_config);
-  APP_ERROR_CHECK(error_code);
+    // loop forever
+    while (1) {
+        printf("Sampling...\n");
+        nrf_saadc_value_t sample = sample_value(ADC_CHANNEL);
+        printf("sample: %d\n", sample);
+        nrf_delay_ms(1);
+    }
 
-  // specify input pin and initialize that ADC channel
-  channel_config.pin_p = BUCKLER_ANALOG_ACCEL_Z;
-  error_code = nrfx_saadc_channel_init(Z_CHANNEL, &channel_config);
-  APP_ERROR_CHECK(error_code);
-  
-  // initialization complete
-  printf("Buckler initialized!\n");
-
-  // loop forever
-  while (1) {
-    // sample analog inputs
-    nrf_saadc_value_t x_val = sample_value(X_CHANNEL);
-    nrf_saadc_value_t y_val = sample_value(Y_CHANNEL);
-    nrf_saadc_value_t z_val = sample_value(Z_CHANNEL);
-
-    // display results
-    printf("x: %d\ty: %d\tz:%d\n", x_val, y_val, z_val);
-    nrf_delay_ms(1);
-  }
 }
-
-
