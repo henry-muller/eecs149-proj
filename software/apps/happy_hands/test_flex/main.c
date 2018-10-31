078f4156ca9f5130c4676e9375b5d0913bf43d89
Henry Muller <henrymuller@berkeley.edu>
2018-10-30 17:44:27 -0700

    Create initial code to handle analog inputs

--- software/apps/happy_hands/test_flex/main.c
+++ software/apps/happy_hands/test_flex/main.c
@@ -19,19 +19,42 @@
 
 #include "buckler.h"
 
-int main() {
+#define ADC_CHANNEL 0
 
+int main() {
     ret_code_t error_code = NRF_SUCCESS;
 
-    // initialize GPIO driver
-    if (!nrfx_gpiote_is_init()) {
-        error_code = nrfx_gpiote_init();
-    }
+    // initialize RTT library
+    error_code = NRF_LOG_INIT(NULL);
     APP_ERROR_CHECK(error_code);
+    NRF_LOG_DEFAULT_BACKENDS_INIT();
+
+    // initialize analog to digital converter
+    nrfx_saadc_config_t saadc_config = NRFX_SAADC_DEFAULT_CONFIG;
+    saadc_config.resolution = NRF_SAADC_RESOLUTION_12BIT;
+    error_code = nrfx_saadc_init(&saadc_config, saadc_callback);
+    APP_ERROR_CHECK(error_code);
+
+    // initialize analog inputs
+    // configure with 0 as input pin for now
+    nrf_saadc_channel_config_t channel_config = NRFX_SAADC_DEFAULT_CHANNEL_CONFIG_SE(0);
+    channel_config.gain = NRF_SAADC_GAIN1_6; // input gain of 1/6 Volts/Volt, multiply incoming signal by (1/6)
+    channel_config.reference = NRF_SAADC_REFERENCE_INTERNAL; // 0.6 Volt reference, input after gain can be 0 to 0.6 Volts
+
+     // specify input pin and initialize that ADC channel
+    channel_config.pin_p = NRF_SAADC_INPUT_AIN0;
+    error_code = nrfx_saadc_channel_init(ADC_CHANNEL, &channel_config);
+    APP_ERROR_CHECK(error_code);
+
+    nrf_saadc_input_t AIN0 = NRF_SAADC_INPUT_AIN5; // Analog Input 0 pin: 0.2
+    nrfx_gpiote_in_set(AIN0);
 
     // loop forever
     while (1) {
-        printf("Hello World!\n");
+        printf("Sampling...\n");
+        nrf_saadc_value_t sample = sample_value(ADC_CHANNEL);
+        printf("sample: %d\n", sample);
+        nrf_delay_ms(1);
     }
 
 }
